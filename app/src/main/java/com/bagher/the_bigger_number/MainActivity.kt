d4505b3a88be97f8c797abb4e4577029139941b7
Bagher <bagherhasani0@gmail.com>
2022-12-17 17:00:29 +0330

    this is the first commit

---
+++ app/src/main/java/com/bagher/the_bigger_number/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.bagher.the_bigger_number
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        assignNumbersToButtons()
+
+        btnLeft.setOnClickListener(){
+
+
+            checkAsnwer(isRightButtonSelected = false)
+            assignNumbersToButtons()
+
+        }
+
+        btnRight.setOnClickListener(){
+            checkAsnwer(isRightButtonSelected = true)
+            assignNumbersToButtons()
+        }
+
+
+
+
+    }
+
+    private fun assignNumbersToButtons(){
+
+        val leftNum:Int= Random.nextInt(10)
+        var rightNum:Int=Random.nextInt(10)
+
+        while (leftNum==rightNum){
+            rightNum=Random.nextInt(10)
+        }
+
+        btnRight.text="$rightNum"
+        btnLeft.text="$leftNum"
+
+    }
+
+    private fun checkAsnwer(isRightButtonSelected:Boolean){
+
+        val rightNum:Int=btnRight.text.toString().toInt()
+        val leftNum:Int=btnLeft.text.toString().toInt()
+
+
+        val isAnswerCorrect:Boolean=  if (isRightButtonSelected) rightNum>leftNum else leftNum>rightNum
+
+        if (isAnswerCorrect){
+
+            constraintMain.setBackgroundColor(Color.GREEN)
+            Toast.makeText(this,"Correct",Toast.LENGTH_SHORT).show()
+
+        }else{
+
+            constraintMain.setBackgroundColor(Color.RED)
+            Toast.makeText(this,"Wrong",Toast.LENGTH_SHORT).show()
+
+        }
+
+
+
+    }
+}
